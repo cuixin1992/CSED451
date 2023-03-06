18fe4c7b97974d38e6fbddc67c9b31b29d6c7fb9
unknown <gmldn00@gmail.com>
2023-03-06 15:42:59 +0900

    feat: 1) jumping character 2) moving fireball

--- Source.cpp
+++ Source.cpp
@@ -4,56 +4,102 @@
 #include <glm/vec3.hpp>
 #include <vector>	
 
-typedef struct rect {
-	float x;
-	float y;
-	float width;
-	float height;
-} rect;
+GLfloat jump = 0.0f; // jump height
+GLfloat jumpSpeed = 0.05f; // jump speed
+bool jumping = false; // jumping flag
+int windowWidth = 500; // window width
+int windowHeight = 500; // window height
 
-rect rectangle;
+GLfloat fireball = 1.5f;
+GLfloat fireballSpeed = 0.001f; // fireball speed
 
-void init(void) {
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-	glShadeModel(GL_FLAT);
-	rectangle.x = 0.1;
-	rectangle.y = 0.1;
-	rectangle.width = 0.1;
-	rectangle.height = 0.15;
+void drawRectangle() {
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glBegin(GL_QUADS);
+        glVertex2f(-0.1f, 0.1f);
+        glVertex2f(0.1f, 0.1f);
+        glVertex2f(0.1f, -0.1f);
+        glVertex2f(-0.1f, -0.1f);
+    glEnd();
 }
 
-std::vector<glm::vec3> position = {
-glm::vec3(0.0f, 0.0f, 0.0f),
-glm::vec3(0.0f, 1.0f, 0.0f),
-glm::vec3(1.0f, 1.0f, 0.0f),
-glm::vec3(1.0f, 0.0f, 0.0f),
-};
-
-void display(void) {
-	glClear(GL_COLOR_BUFFER_BIT);
-	glColor3f(1.0f, 0.0f, 0.0f);
-	glBegin(GL_POLYGON);
-		glVertex2f(rectangle.x, rectangle.y);
-		glVertex2f(rectangle.x, rectangle.y + rectangle.width);
-		glVertex2f(rectangle.x + rectangle.height, rectangle.y + rectangle.width);
-		glVertex2f(rectangle.x + rectangle.height, rectangle.y); 
-	glEnd();
-	//glFlush();
-	glutSwapBuffers();
+void drawFireBall() {
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glBegin(GL_QUADS);
+        glVertex2f(-0.05f, 0.05f);
+        glVertex2f(0.05f, 0.05f);
+        glVertex2f(0.05f, -0.05f);
+        glVertex2f(-0.05f, -0.05f);
+    glEnd();
 }
-void main(int argc, char** argv) {
-	// inspect array elements and print to console
-	const float* probe = &position[0].x;
-	for (size_t i = 0; i < position.size() * 3; i++) {
-		std::cout << probe[i] << std::endl;
-	}
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
-	glutInitWindowPosition(100, 100);
-	glutInitWindowSize(320, 320);
-	glutCreateWindow("Hello OpenGL");
-	glutDisplayFunc(display);
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glPushMatrix();
+        glTranslatef(0.0f, jump, 0.0f);
+        drawRectangle();
+    glPopMatrix();
 
-	glewInit();
-	glutMainLoop();
+    glPushMatrix();
+        glTranslatef(fireball, 0.0f, 0.0f);
+        drawFireBall();
+    glPopMatrix();
+
+    glutSwapBuffers();
+}
+
+void update(int value) {
+    if (jumping) {
+        if (jump < 1.0f) {
+            jump += jumpSpeed;
+        }
+        else {
+            jumping = false;
+        }
+    }
+    else {
+        if (jump > 0.0f) {
+            jump -= jumpSpeed;
+        }
+    }
+
+    fireball -= fireballSpeed;
+    glutPostRedisplay();
+    glutTimerFunc(10, update, 0);
+}
+
+void keyboard(unsigned char key, int x, int y) {
+    if (key == ' ') {
+        jumping = true;
+    }
+}
+
+void reshape(int width, int height) {
+    windowWidth = width;
+    windowHeight = height;
+    glViewport(0, 0, windowWidth, windowHeight);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    if (windowWidth <= windowHeight) {
+        GLfloat aspect = (GLfloat)windowHeight / (GLfloat)windowWidth;
+        glOrtho(-1.0, 1.0, -1.0 * aspect, 1.0 * aspect, -1.0, 1.0);
+    }
+    else {
+        GLfloat aspect = (GLfloat)windowWidth / (GLfloat)windowHeight;
+        glOrtho(-1.0 * aspect, 1.0 * aspect, -1.0, 1.0, -1.0, 1.0);
+    }
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE);
+    glutInitWindowSize(500, 500);
+    glutCreateWindow("Jumping Rectangle");
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutKeyboardFunc(keyboard);
+    glutTimerFunc(10, update, 0);
+    glutMainLoop();
 }
